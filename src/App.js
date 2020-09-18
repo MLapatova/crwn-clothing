13aecf090165fc4ded5eb7420f52b60bc86ae57a
MLapatova <marieta.lapatova@abv.bg>
2020-09-18 21:27:33 +0300

    changes for shop page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
+
 import './App.css';
 
-import HomePage from '../src/pages/homepage/homepage.component'
+import HomePage from '../src/pages/homepage/homepage.component';
+import ShopPage from '../src/pages/shop/shop.component';
+
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
